37ce7803c776719106e5316119bb58947a355fdb
jeuceda <jeuceda8@gmail.com>
2023-07-19 11:37:48 -0600

    Archivo Inicial con desarrollo

---
+++ variables.py
@@ -0,0 +1,20 @@
+edad = 29
+nombre = 'Josias Euceda'
+altura = 1.77
+VoF = True
+
+variable_1 = 'Mi nombre es ' + nombre + ' mi edad es: ' + str(edad) + ' años y mi altura es de ' + str(altura) + 'metros.'
+
+'''
+El límite máximo de un entero en Python 3.0 no esta restringido y puede expandirse a los limites disponibles de memoria
+
+El valor maximo de cualquier floating-point number en python es 1.8 x 10e308, ya despues de eso python lo indica como inf.
+'''
+
+n = int(input('Ingresa un numero: '))
+
+suma = n * (n + 1 )
+
+print(variable_1)
+print (suma)
+
